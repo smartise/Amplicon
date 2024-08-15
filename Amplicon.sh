70ad4010ef96b75099c4b26960cda2474c99f319
smartise <olivier1996collard@hotmail.fr>
2024-08-15 15:03:26 +0200

    adding new features

--- Amplicon.sh
+++ Amplicon.sh
@@ -1,39 +1,70 @@
-##########################################
-#### Basecalling from the fast5 folder ###
-##########################################
+screen -r -d Basecalling$
+conda activate amplicon_sorter
+cd /mnt/ebe/AmpliconSequencingONT/
+
+read -p "Please enter the name of the experiment (be sure to tipe well):" filename
+
+mkdir "$filename"
+
+mkdir "$filename"/fast5
+
+echo "##########################################"
+echo "### Downloading the files              ###"
+echo "##########################################"
+password="026502925"
+read -p "Where the sequencing was done ? (p2-post/min-post)" answer
+if [[ "$answer" == "p2-post" ]]; then
+    echo "downloading from P2 solo post" 
+    sshpass -p "$password" scp promethion@10.141.4.16/var/lib/minknow/data/"$filename"/*/*/fast5_fail/*.fast5 /"$filename"/fast5
+    sshpass -p "$password" scp promethion@10.141.4.16/var/lib/minknow/data/"$filename"/*/*/fast5_pass/*.fast5 /"$filename"/fast5
+
+elif [[ "$answer" == "mini-post" ]]; then
+    echo "downloading from mini-post"
+    sshpass -p "$password" scp minit@10.141.4.15/data/"$filename"/*/*/fast5_fail/*.fast5 /"$filename"/fast5
+    sshpass -p "$password" scp minit@10.141.4.15/data/"$filename"/*/*/fast5_pass/*.fast5 /"$filename"/fast5
+
+else
+    echo "Invalid input. Please enter 'p2-post' or 'mini-post'."
+fi
+
+echo "##########################################"
+echo "#### Basecalling from the fast5 folder ###"
+echo "##########################################"
+
+cd "$filename"
 
 module load guppy/gpu-6.3.8_linux64
 
 guppy_basecaller -i ./fast5 -s ./fastq_guppy_6.3.8_8.2 -c dna_r10.4.1_e8.2_400bps_sup.cfg -x auto
 
-##########################################
-#### Barcoding the fastq files         ###
-##########################################
+echo "##########################################"
+echo "#### Barcoding the fastq files         ###"
+echo "##########################################"
 
 guppy_barcoder -i fastq_guppy_6.3.8_8.2/pass/ -s fastq_guppy_6.3.8_8.2_barcodes
 
-##########################################
-#### Using amplicon sorter             ###
-##########################################
+echo "##########################################"
+echo "#### Using amplicon sorter             ###"
+echo "##########################################"
 
 cd fastq_guppy_6.3.8_8.2_barcodes
 
 (for i in {1..96}; do cat $(printf 'barcode%02d\n' $i)/*.fastq > $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq; python3 amplicon_sorter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq -ho -np 40 -o $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i); python3 amplicon_sorter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq -min 200 -np 40 -o $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i); rm $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq; done) & disown
 
-read -p "Do you want to proceed with the command? (yes/no): " answer
+read -p "Do you want use hairsplitter? (yes/no): " answer
 
 if [[ "$answer" == "yes" ]]; then
     # Put your command here
     echo "Executing the command..."
-    ##########################################
-    #### Using hairsplitter                ###
-    ##########################################
+    echo "##########################################"
+    echo "#### Using hairsplitter                ###"
+    echo "##########################################"
 
     for i in {1..96}; do python /mnt/ebe/Hairsplitter/hairsplitter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d_consensussequences\n' $i).fasta -f $(printf 'barcode%02d\n' $i)/$(printf 'fastq_runid_*_0\n' $i).fastq -o $(printf 'barcode%02d\n' $i)/$(printf 'hairsplitter_barcode%02d\n' $i) -F -x amplicon; done
 
-    ##########################################
-    #### put all in one file               ###
-    ##########################################
+    echo "##########################################"
+    echo "#### put all in one file               ###"
+    echo "##########################################"
 
     for i in {1..96}; do cat `printf 'barcode%02d\n' $i`/`printf 'hairsplitter_barcode%02d\n' $i`/hairsplitter_final_assembly.fasta >> alldatahairsplitted.fasta ;done
 
