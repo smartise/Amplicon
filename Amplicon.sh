f492c4f8086724ac1bc8340ac5b3158f7a487dd1
smartise <olivier1996collard@hotmail.fr>
2024-08-23 09:56:15 +0200

    adding choice selection

--- Amplicon.sh
+++ Amplicon.sh
@@ -17,20 +17,37 @@ echo "### Downloading the files              ###"
 echo "##########################################"
 
 password="026502925"
-read -p "Where the sequencing was done ? (p2-post/min-post)" answer
-if [[ "$answer" == "p2-post" ]]; then
-    echo "downloading from P2 solo post" 
-    sshpass -p "$password" scp promethion@10.141.4.16/var/lib/minknow/data/"$filename"/*/*/fast5_fail/*.fast5 /"$filename"/fast5
-    sshpass -p "$password" scp promethion@10.141.4.16/var/lib/minknow/data/"$filename"/*/*/fast5_pass/*.fast5 /"$filename"/fast5
 
-elif [[ "$answer" == "mini-post" ]]; then
-    echo "downloading from mini-post"
-    sshpass -p "$password" scp minit@10.141.4.15/data/"$filename"/*/*/fast5_fail/*.fast5 /"$filename"/fast5
-    sshpass -p "$password" scp minit@10.141.4.15/data/"$filename"/*/*/fast5_pass/*.fast5 /"$filename"/fast5
+echo "Please choose an option from the list below:"
 
-else
-    echo "Invalid input. Please enter 'p2-post' or 'mini-post'."
-fi
+options=("p2-post" "min-post" "Quit")
+
+# Display the options and prompt for a choice
+select choice in "${options[@]}"; do
+    case $choice in
+        "p2-post")
+            echo "You selected p2-post"
+            echo "downloading from P2 solo post" 
+            sshpass -p "$password" scp promethion@10.141.4.16/var/lib/minknow/data/"$filename"/*/*/fast5_fail/*.fast5 /"$filename"/fast5
+            sshpass -p "$password" scp promethion@10.141.4.16/var/lib/minknow/data/"$filename"/*/*/fast5_pass/*.fast5 /"$filename"/fast5
+            break
+            ;;
+        "min-post")
+            echo "You selected min-post"
+            echo "downloading from mini-post"
+            sshpass -p "$password" scp minit@10.141.4.15/data/"$filename"/*/*/fast5_fail/*.fast5 /"$filename"/fast5
+            sshpass -p "$password" scp minit@10.141.4.15/data/"$filename"/*/*/fast5_pass/*.fast5 /"$filename"/fast5
+            break
+            ;;
+        "Quit")
+            echo "Exiting the script."
+            exit 0
+            ;;
+        *)  # Default case if no valid option is chosen
+            echo "Invalid option. Please try again."
+            ;;
+    esac
+done
 
 # Basecalling and barcoding of the data set 
 
@@ -56,7 +73,7 @@ echo "##########################################"
 
 cd fastq_guppy_6.3.8_8.2_barcodes
 
-(for i in {1..96}; do cat $(printf 'barcode%02d\n' $i)/*.fastq > $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq; python3 amplicon_sorter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq -ho -np 40 -o $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i); python3 amplicon_sorter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq -min 200 -np 40 -o $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i); rm $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq; done) & disown
+(for i in {1..96}; do cat $(printf 'barcode%02d\n' $i)/*.fastq > $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq; python3 amplicon_sorter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq -ho -np 40 -o $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i); python3 amplicon_sorter.py -i $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq -min 200 -np 40 -o $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i); rm $(printf 'barcode%02d\n' $i)/$(printf 'barcode%02d\n' $i).fastq; done) 
 
 # doing the Hairsplitter if necessar 
 
