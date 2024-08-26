ebc7a4115d710609a6d80cec874045b320f2a173
smartise <olivier1996collard@hotmail.fr>
2024-08-26 11:14:35 +0200

    jkjh

--- Amplicon_minit-post.sh
+++ Amplicon_minit-post.sh
@@ -7,7 +7,7 @@ echo "The experiment is, $filename!"
 
 ssh "ocol0007@ebe-gpu01.hpda.ulb.ac.be" "mkdir -p '/mnt/ebe/AmpliconSequencingONT/$filename/fast5'"
 
-scp data/$filename/*/*/fast5_fail/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
-scp data/$filename/*/*/fast5_pass/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
+scp /data/$filename/*/*/fast5_fail/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
+scp /ata/$filename/*/*/fast5_pass/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
 
 ssh "ocol0007@ebe-gpu01.hpda.ulb.ac.be" 'bash /srv/home/ocol0007/Amplicon/Amplicon.sh '$filename''
