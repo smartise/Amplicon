9c0f87768a049a5ef94cf248c1701f9178f3dcd3
smartise <olivier1996collard@hotmail.fr>
2024-08-26 11:01:11 +0200

    yo

--- Amplicon_minit-post.sh
+++ Amplicon_minit-post.sh
@@ -3,6 +3,8 @@
 
 filename=$(ls -lt --time=ctime "/data" | grep '^d' | head -n 1 | awk '{print $NF}')
 
+echo "the taken file is $filemname"
+
 ssh "ocol0007@ebe-gpu01.hpda.ulb.ac.be" "mkdir -p '/mnt/ebe/AmpliconSequencingONT/$filename/fast5'"
 
 scp data/$filename/*/*/fast5_fail/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
