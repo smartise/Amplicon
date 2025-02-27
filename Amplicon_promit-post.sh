d8d6adae4e994fc89124bcf6aaf3b22dead4ce20
smartise <olivier1996collard@hotmail.fr>
2025-02-27 09:47:44 +0100

    jnj

--- Amplicon_promit-post.sh
+++ Amplicon_promit-post.sh
@@ -6,6 +6,6 @@ echo "The experiment is, $filename!"
 
 ssh "ocol0007@ebe-gpu01.hpda.ulb.ac.be" "mkdir -p '/mnt/ebe/AmpliconSequencingONT/$filename/fast5'"
 
-scp /var/lib/minknow/data/$filename/*/*/fast5_fail/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
-scp /var/lib/minknow/data/$filename/*/*/fast5_pass/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
-scp /var/lib/minknow/data/$filename/*/*/fast5_skip/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
+scp /var/lib/minknow/data/$filename/*/*/fast5_fail/unclassified/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
+scp /var/lib/minknow/data/$filename/*/*/fast5_pass/unclassified/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
+scp /var/lib/minknow/data/$filename/*/*/fast5_skip/unclassified/*.fast5 ocol0007@ebe-gpu01.hpda.ulb.ac.be:/mnt/ebe/AmpliconSequencingONT/$filename/fast5
